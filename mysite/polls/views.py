5be951a2e48666ffd7e22d4ec174b59d4b4a60ab
Hazlewood, Samuel (sh931a) <sh931a@att.com>
2022-01-03 08:13:44 -0600

    Django Polls App - Half of tut 3, views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello World. Polls Index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list':latest_question_list,
+    }
+    return render(request, 'polls/index.html',context)
+    #render shortcut: takes request, template, dict (mapping template var
+    # to python var)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the result of question {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
     
+
 # Create your views here.
